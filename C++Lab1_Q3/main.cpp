d4f9f3f9aaccc3ebe8c368379282b344c7555ca8
v3eti <Veeti.Klof@metropolia.fi>
2025-04-30 22:33:50 +0300

    Add files via upload

---
+++ C++Lab1_Q3/main.cpp
@@ -0,0 +1,21 @@
+//Lab1T3
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*Task 3
+1. easy?
+2. suitable variable type for loop? like short?*/
+
+
+char n1 = 50;
+
+for (short i =2; i <= n1; i += 2){
+ cout << i << " ";
+
+}
+
+    return 0;
+}
