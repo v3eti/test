879ccfd3060d1ca230c80edf14260b4464fc4957
v3eti <Veeti.Klof@metropolia.fi>
2025-04-30 22:30:58 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,68 +1,35 @@
-#include <iostream>//Lab2E1
-#include <cmath>
+//Lab1E2
+#include <iostream>
+#include <string>
+#include <algorithm>
+
 using namespace std;
-int num1, num2;
-double dub1, dub2;
-bool roundTF = 0;
-///int prdt=1;
-//A
-void multiply(int a, int b){ //mutiply function that multiplyes imputs 1 and 2 together
-cout << "product = " << (a * b) << "\n";
+
+int main()
+{
+//getline
+short amount=0;
+string he;
+    cout <<"Enter Message: ";
+    std::getline(std::cin,he); // get imput from user
+short l = he.length();
+transform(he.begin(), he.end(), he.begin(),::toupper); //Transforms all the lower case chareters.
+
+for (int i=0; i<l; i++){ //Find the vowels
+if ((((he[i]=='A' || he[i]=='E') || he[i]=='I') || he[i]=='O') || he[i]=='U')
+    amount++;
+
+
 }
 
 
-void multiply(double a, double b){ // overloaded double version of multiply function
-      cout << "product = " << (a * b) << "\n";
+for (int i=0; i<l/2; i++){ //loops for half of the amount of letters
+    swap(he[i], he[l-i-1]); // swaps the farthest appart non swaped chareters
 }
 
-void multiply(int a, double b){ // overloaded double version of multiply function
-      if (roundTF >= 1){
-//if 1 than round
-      cout << "product rounded = " << round(a * b) << "\n";
-
-}else{
-
-         cout << "product = " << (a * b) << "\n";
-}
-}
-int main(){
-    //get intigers
-    cout <<"Enter first integer: ";
-cin >> num1; // get input from user
-
-
-    cout <<"Enter second integer: ";
-cin >> num2; // get input from user
-
-      multiply(num1,num2);
-
-         cout << "A Done." << endl;
-
-         //get dubles
-    cout <<"Enter first double: ";
-cin >> dub1; // get input from user
-
-
-    cout <<"Enter second double: ";
-cin >> dub2; // get input from user
-
-      multiply(dub1,dub2);
-
-   cout << "B Done." << endl;
-
-
-       //get intiger
-    cout <<"Enter first integer: ";
-cin >> num1; // get input from user
-//get duble
-    cout <<"Enter second double: ";
-cin >> dub2; // get input from user
-//Get bool
-   cout <<"Round 1=yes, 0=no: ";
-cin >> roundTF; // get input from user
-
-  multiply(num1,dub2);
-
-
+std::cout << "The mount of vowels:" << " " << amount << endl; //
+std::cout << he << endl;
     return 0;
 }
+
+
