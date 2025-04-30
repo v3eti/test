eb3774e6961b76e85b7620ac47dd03e5ed77242e
v3eti <Veeti.Klof@metropolia.fi>
2025-04-30 22:34:16 +0300

    Add files via upload

---
+++ C++Lab1_Q4/main.cpp
@@ -0,0 +1,21 @@
+/*Lab1E4
+Find a get an imput from user command as an integer and square it hophelly with a command and not a manual one.num1
+Print output.*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int x;
+int sqr;
+    cout <<"Enter a number to square: ";
+    cin >> sqr; // get imput from user
+
+    int squared = sqr * sqr;
+  std::cout << sqr << " squared is " << squared << std::endl;
+    return 0;
+}
+// RE make the hole thing using macro
+
