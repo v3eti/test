25aedc814dd1bd56b845d83269639ff6f857c973
v3eti <Veeti.Klof@metropolia.fi>
2025-04-30 22:36:37 +0300

    Add files via upload

---
+++ C++Lab2E2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>//Lab2E2
+
+using namespace std;
+void swapf()
+float
+int a, b
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
+//Goal is to swap float values
+/*
+without pointer or references
+• pointers: void swapUsingPointers(int *a, int *b);
+• references: void swapUsingReferences(int &a, int &b);
+
+
+string fullName;
+cout << "Type your full name: ";
+getline (cin, fullName);
+cout << "Your name is: " << fullName;
+*/
